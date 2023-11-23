aaee79e09dd95918b6f3b98bfdd3d32d5b5b9d7a
Kazutadashi <owenvl@yahoo.com>
2023-11-23 01:28:36 -0700

    Add more variables for players

--- fear_server_utils.py
+++ fear_server_utils.py
@@ -17,22 +17,30 @@ def load_world(log_file_line):
 def set_current_world():
     server_status['loading_world_flag'] = 0
     server_status['current_world'] = server_status['world_being_loaded']
+    server_status['world_being_loaded'] = 'None'
 
 
 def connect_player(log_file_line):
-    player_details = log_file_line.split('] ')
-    print(player_details)
-    server_status['players_connected'].append(player_details[3][1:])  # using [1:] here to remove the first '[' char
+    player_details = log_file_line.split('] ')  # splits the server output into columns
+    server_status['players_connected'].append({
+        'game_name': player_details[3][1:],
+        'site_name': 'DISPLAY NAME',
+        'ping': 100
+    })  # using [1:] here to remove the first '[' char
 
 
 def disconnect_player(log_file_line):
-    player_details = log_file_line.split('] ')
-    print(player_details)
-    player = player_details[3][1:]
+    player_details = log_file_line.split('] ')  # splits the server output into columns
+    player = player_details[3][1:]  # using [1:] here to remove the first '[' char (gets the username)
     try:
-        server_status['players_connected'].remove(player)
+        # we basically just rebuild the list of dicts here with this comprehension, not including
+        # the one that needs to be removed.
+        server_status['players_connected'] =\
+            [player_dict for player_dict in server_status['players_connected'] if player_dict['game_name'] != player]
     except ValueError as emsg:
-        print(f'[WARNING] Player {player} disconnected without ever connecting.\n{emsg})')
+        # There seems to be a bug in the linux server software that allows a player to connect without
+        # showing up in the logs. Not sure why this happens.
+        print(f'[WARNING] Player {player} somehow disconnected without ever connecting.\n{emsg})')
 
 
 server_log = open('/home/kazutadashi-lt/Desktop/11052023.log', 'r', errors='replace')
@@ -46,10 +54,14 @@ server_status = {
 }
 
 
-for line in server_log:
-    print(line)
 
-    # Check to see if the world load line was entered
+for line in server_log:
+    # print(line)
+    split_line = line.split(']')  # some things will need the position of the column of output instead
+    # TODO: maybe change all of these things to be column based instead of position VV
+
+    # We hard code the locations of these keywords to prevent accidental
+    # collision with usernames
     if line[0:13] == 'Loading world':
         load_world(line)
 
@@ -59,9 +71,13 @@ for line in server_log:
     if line[-17:-1] == 'Client connected':  # remove the last two chars to avoid newline char
         connect_player(line)
 
-    print(line[-20:-1])
     if line[-20:-1] == 'Client disconnected':  # remove the last two chars to avoid newline char
         disconnect_player(line)
+
+    # Harder to hardcode this one because the position of [CHAT] is variable
+    if '[CHAT]' in line:  # TODO: this is dangerous because a player can name themselves chat and mess up stats
+        print("this is chat")
+
 
     print(server_status)
 
