56259363b62559acf88dbb250197b7c815af21f5
Kazutadashi <owenvl@yahoo.com>
2023-11-23 19:18:25 -0700

    Add display name retriever function

--- fear_server_utils.py
+++ fear_server_utils.py
@@ -6,6 +6,8 @@ LOADING_WORLD_PREFIX = 'Loading world'
 WORLD_LOADED_PREFIX = 'World loaded'
 CLIENT_CONNECTED_SUFFIX = 'Client connected\n'
 CLIENT_DISCONNECTED_SUFFIX = 'Client disconnected\n'
+DISPLAY_NAME_PATTERN = '\[INFO\].*-- Display Name:'
+
 
 def load_world(log_file_line):
     server_status['loading_world_flag'] = 1
@@ -28,11 +30,13 @@ def set_current_world():
 
 def connect_player(log_file_line):
     player_details = log_file_line.split(']')  # splits the server output into columns
+
+    # We use [2:] to remove the first ' [' chars
     server_status['players_connected'].append({
-        'game_name': player_details[3][1:],
-        'connect_time': player_details[0][1:],
-        'ip_port': player_details[1][1:],
-        'ping': player_details[2][1:],
+        'game_name': player_details[3][2:],
+        'connect_time': player_details[0][2:],
+        'ip_port': player_details[1][2:],
+        'ping': player_details[2][2:],
         'site_name': '',
         'sec2_cd_verified': '',
         'guid': ''
@@ -41,7 +45,7 @@ def connect_player(log_file_line):
 
 def disconnect_player(log_file_line):
     player_details = log_file_line.split(']')  # splits the server output into columns
-    player = player_details[3][1:]  # using [1:] here to remove the first '[' char (gets the username)
+    player = player_details[3][2:]  # using [2:] here to remove the first ' [' chars (gets the username)
     try:
         # we basically just rebuild the list of dicts here with this comprehension, not including
         # the one that needs to be removed.
@@ -51,6 +55,25 @@ def disconnect_player(log_file_line):
         # There seems to be a bug in the linux server software that allows a player to connect without
         # showing up in the logs. Not sure why this happens.
         print(f'[WARNING] Player {player} somehow disconnected without ever connecting.\n{emsg})')
+
+
+def add_display_name(log_file_line):
+
+    player_details = log_file_line.split(']')
+    game_name = player_details[3][2:]  # remove the leading space and '[' character to get the name
+
+    display_name_pattern = r'-- Display Name:\s*(\S+)'
+    match = re.search(display_name_pattern, log_file_line)
+
+    if match:
+        for player in server_status['players_connected']:
+            if player['game_name'] == game_name:
+                player['site_name'] = match.group(1).strip()
+            else:
+                # TODO: splitting by ']' causes players with names like "[RaW]coolguy" to get split up as well and cause problems.
+                player['site_name'] = 'NAME MISMATCH!'
+    else:
+        name = None
 
 
 server_log = open('/home/kazutadashi-lt/Desktop/11052023.log', 'r', errors='replace')
@@ -66,24 +89,29 @@ server_status = {
 
 
 for line in server_log:
-    # print(line)
-    split_line = line.split(']')  # some things will need the position of the column of output instead
-    # TODO: maybe change all of these things to be column based instead of position VV
-
     # We hard code the locations of these keywords to prevent accidental
     # collision with usernames
+    print(line)
 
     if line.startswith(LOADING_WORLD_PREFIX):
         load_world(line)
+        continue
 
     if line.startswith(WORLD_LOADED_PREFIX) and server_status['loading_world_flag'] == 1:
         set_current_world()
+        continue
 
     if line.endswith(CLIENT_CONNECTED_SUFFIX):
         connect_player(line)
+        continue
 
     if line.endswith(CLIENT_DISCONNECTED_SUFFIX):
         disconnect_player(line)
+        continue
+
+    if re.search(DISPLAY_NAME_PATTERN, line):  # If the line has -- Display Name: in it
+        add_display_name(line)
+        continue
 
     # Harder to hardcode this one because the position of [CHAT] is variable
     if '[CHAT]' in line:  # TODO: this is dangerous because a player can name themselves chat and mess up stats
